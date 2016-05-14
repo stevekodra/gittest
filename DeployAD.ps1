4e498eb4ac36ba41924bbc5ec7f96fb76d556373
steve-kodra <steve.kodra@travelex.com>
2016-05-14 16:37:56 +0100

    powershell
    changes on ad

--- DeployAD.ps1
+++ DeployAD.ps1
@@ -4,8 +4,8 @@
 # COMMENT : This script install Active Directory role
 # VERSION : 1.0
 ###########################################################
-# ERROR REPORTING ALL
-Rename-Computer AWSAD11 -restart
+#ERROR REPORTING ALL
+#Rename-Computer AWSAD11 -restart
 Set-StrictMode -Version latest
 #Import PowerShell Module 
 Try
@@ -20,15 +20,18 @@ Catch
 #Install Windows Backup and SNMP Features
 try
 {
-Install-WindowsFeature SNMP-Service, SNMP-WMI-Provider, Windows-Server-Backup -Verbose -Confirm
+Install-WindowsFeature SNMP-Service, SNMP-WMI-Provider, Windows-Server-Backup -ErrorAction SilentlyContinue
 }
 Catch
 {
     Write-Host "[ERROR] Couldn't be install feature. Script will stop!"
 }
 #Input Domain Name
-$dcname = Read-Host "Digital.travelex.net"
+$dcname = "Digital.travelex.net"
+$netbios = "Digital"
+$SecurePassword = "P@ssw0rd" | ConvertTo-SecureString -AsPlainText -Force
 #Install AD-DS role
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Verbose
+Import-Module ADDSDeployment
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Verbose -ErrorAction SilentlyContinue
 #Create forest and install ad dc,dns
-Install-ADDSForest -domainname $dcname -DomainMode Win2012R2 -ForestMode Win2012R2 -NoRebootOnCompletion:$false -Confirm -Verbose
+Install-ADDSForest -domainname $dcname -DomainMode Win2012R2 -ForestMode Win2012R2  -InstallDns:$true -DomainNetbiosName $netbios -SafeModeAdministratorPassword $SecurePassword  -NoRebootOnCompletion:$false -Force
