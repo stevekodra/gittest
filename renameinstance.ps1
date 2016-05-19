b27c88121791e4f98e08f433549cbca199d4088e
steve-kodra <steve.kodra@travelex.com>
2016-05-19 16:32:21 +0100

    updatged

--- renameinstance.ps1
+++ renameinstance.ps1
@@ -9,7 +9,7 @@ Rename-Computer AWSAD01
 # Sets a Registery Key & also execute the Script to promote to domain controller.
  
 $RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-set-itemproperty $RunOnceKey "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -NoExit -executionPolicy Unrestricted -File ' + "C:\users\DeployAD.ps1" )
+set-itemproperty $RunOnceKey "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -NoExit -executionPolicy Unrestricted -File ' + "C:\DeployAD.ps1" )
 
 # Allows the Instance to save above registery 
 Start-Sleep -Seconds 10
