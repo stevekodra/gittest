1fb3c7418c35e27043f010bbef285c70417f009c
steve-kodra <steve.kodra@travelex.com>
2016-05-19 16:18:13 +0100

    RenameInstance

---
+++ renameinstance.ps1
@@ -0,0 +1,18 @@
+﻿
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
+# This Script Renames the instance to AWSAD01 and Also it schedule Job for another script to promote it to domain controller
+
+# Rename the computer
+Rename-Computer AWSAD01
+
+# Sets a Registery Key & also execute the Script to promote to domain controller.
+ 
+$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+set-itemproperty $RunOnceKey "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -NoExit -executionPolicy Unrestricted -File ' + "C:\users\DeployAD.ps1" )
+
+# Allows the Instance to save above registery 
+Start-Sleep -Seconds 10
+
+# Restarts the computer 
+Restart-Computer
