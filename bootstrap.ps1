c09f62eb7be9bcf070e4698eeec6a875e56a2643
steve-kodra <steve.kodra@travelex.com>
2016-05-19 17:10:24 +0100

    botstrap

---
+++ bootstrap.ps1
@@ -0,0 +1,12 @@
+﻿
+
+          
+          "$source = \"https://raw.githubusercontent.com/stevekodra/gittest/testinggit/DeployAD.ps1\"\n",
+          "$destination = \"C:\\Scripts\\DeployAD.ps1\"\n",
+          "Invoke-WebRequest $source -OutFile $destination \n",
+          
+          "$source1 = \"https://raw.githubusercontent.com/stevekodra/gittest/testinggit/DeployAD.ps1\"\n",
+          "$destination1 = \"C:\\Scripts\\DeployAD.ps1\"\n",
+          "Invoke-WebRequest $source -OutFile $destination1 \n",
+          "$location = & 'C:\\Program Files\\Amazon\\Ec2ConfigService\\Scripts\\DeployAD.ps1' \n",
+          "Invoke-Expression $location\n",
